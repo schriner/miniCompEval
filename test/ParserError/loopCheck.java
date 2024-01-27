608546dbfa2f83aecfddc34b3add41939f3016b5
Sonya <29043296+schriner@users.noreply.github.com>
2024-01-27 16:18:20 -0500

    print line at which type error occurs

---
+++ test/ParserError/loopCheck.java
@@ -0,0 +1,13 @@
+// You are supposed to support method calling for testing this case.
+// The minimum required would be calling a method with an empty parameter list and belongs to a class without instance variables.
+class loop {
+    public static void main(String [] args) {
+		int a;
+		bool b;
+        {
+        System.out.print("Factorial of 10 is:");
+        System.out.println(new Factorial().compute10());
+        }
+    }
+}
+
