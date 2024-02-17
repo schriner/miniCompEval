bd0e472d3919d862e1bbb907774db2e60f7ec6f8
Sonya <29043296+schriner@users.noreply.github.com>
2024-02-16 23:55:16 -0500

    Cmake

--- source/LLVM.cpp
+++ source/LLVM.cpp
@@ -9,9 +9,16 @@
 
 #include "TreeNode.hpp"
 #include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 
 using namespace std;
+using namespace llvm;
 
 void GenerateIR() {
-	
+	LLVMContext Context;
+
+  // Create some module to put our function into it.
+	std::unique_ptr<Module> Owner(new Module("test", Context));
+	Module *M = Owner.get();
+
 }
