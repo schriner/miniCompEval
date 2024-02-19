77412a082b276ede169b503da29576a5691d9561
Sonya <29043296+schriner@users.noreply.github.com>
2024-02-19 14:35:42 -0500

    test/Compiler/multiplePrint.java

--- test/Compiler/multiplePrint.java
+++ test/Compiler/multiplePrint.java
@@ -1,14 +1,14 @@
 // This example involves string literal printing, integer printing,
 // and if-else branch execution.
 // Indicated by the else branch, your interpretation should not print the line in it.
-class multiplePrint {
+class simple {
     public static void main(String [] args) {
 			{
             System.out.println("The answer");
             System.out.println("to the meaning");
             System.out.println("of life");
-            System.out.println("is: ");
-            System.out.print(42);
+            System.out.println("is:");
+            System.out.println("42");
 			}
     }
 }
