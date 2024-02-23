7fc14f2759141ebe8519b7dfb51990abe830e6f0
Sonya <29043296+schriner@users.noreply.github.com>
2024-02-23 13:49:19 -0500

    test_llvm

--- test/test_llvm.sh
+++ test/test_llvm.sh
@@ -6,18 +6,37 @@
 # File: test_llvm.sh 
 #
 
+if [ ! -f $1 ]; then
+	echo "Test Failure: $1 does not exist"
+	exit -1
+fi
+
 TEST_PATH=${0%/*}
 TEST_DIR=${1%/*}
-OUT_DIR=$TEST_PATH/LLVM_IR
+OUT_DIR=$TEST_PATH/tmpLLVM_IR
 BUILD_DIR=$TEST_PATH/../build
 file=$(basename -- "$1")
 
+mkdir $OUT_DIR
+
 $BUILD_DIR/mjavacllvm -o $OUT_DIR/"${file%.*}.bc" $1
+if [ ! -f $OUT_DIR/"${file%.*}.bc" ]; then
+	echo
+	echo "Test Failure: $OUT_DIR/${file%.*}.bc was not built"
+	rm -r $OUT_DIR
+	exit -1
+fi
 clang $OUT_DIR/${file%.*}.bc -o $OUT_DIR/${file%.*}
 javac $1
 
 echo
 pushd $TEST_DIR > /dev/null 3>&1
+if [ ! -f "${file%.*}.class" ]; then
+	echo
+	echo "Test Failure: ${file%.*}.class was not built"
+	rm -r $OUT_DIR
+	exit -1
+fi
 java ${file%.*} > tmpjavac
 popd > /dev/null 2>&1
 $OUT_DIR/${file%.*} > tmpllvm
@@ -27,7 +46,7 @@ error=$?
 if [ $error -eq 0 ]
 then
 	echo "Success"
-	rm $OUT_DIR/${file%.*} tmpllvm $TEST_DIR/tmpjavac
+	rm -r $OUT_DIR tmpllvm $TEST_DIR/tmpjavac
 elif [ $error -eq 1 ]
 then
 	echo "Test Failure: ${file%.*}"
